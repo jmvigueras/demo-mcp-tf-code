624fe1558b5cc7e02b52415470a53d8a1edcdd9c
fortidemoscloud <fortidemoscloud@proton.me>
2025-06-05 16:38:20 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+locals {
+  # General variables
+  prefix = "mcp-demo"
+}
+
+#--------------------------------------------------------------------------------------------------------------
+# FGT Cluster module example
+# - 1 FortiGate cluster FGCP in 2 AZ
+#--------------------------------------------------------------------------------------------------------------
+module "fgt-cluster" {
+  source  = "jmvigueras/ftnt-aws-modules/aws//examples/basic_fgt-cluster"
+  version = "0.0.12"
+
+  prefix = local.prefix
+
+  region = "eu-west-1"
+  azs    = ["eu-west-1a"]
+
+  fgt_build    = "build2795"
+  license_type = "payg"
+
+  fgt_number_peer_az = 1
+  fgt_cluster_type   = "fgcp"
+
+  public_subnet_names_extra  = ["bastion"]
+  private_subnet_names_extra = ["protected"]
+}
+
+
+output "fgt" {
+  value = module.fgt-cluster.fgt
+}
+
+locals {
+}
